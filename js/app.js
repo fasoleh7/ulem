2bfd87d3d4276c20aab4bd0d5386fabeabc401a0
Fathurrohman Solehudin <52761218+fasoleh7@users.noreply.github.com>
2024-02-04 00:35:56 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -259,7 +259,7 @@ const progress = (() => {
     const progress = () => {
         loaded += 1;
 
-        bar.style.width = Math.min((loaded / total) * 1000, 100).toString() + "%";
+        bar.style.width = Math.min((loaded / total) * 100, 100).toString() + "%";
         info.innerText = `Loading assets (${loaded}/${total}) [${parseInt(bar.style.width).toFixed(0)}%]`;
 
         if (loaded == total) {
